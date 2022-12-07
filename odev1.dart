d55267ffcba486ae8d8f096329040a5c8685339c
aslihanguven <98274590+aslihanguven@users.noreply.github.com>
2022-12-07 10:03:05 +0300

    Dart ile Değişken Tanımlama
    Bootcamp eğitimi 1.ödev

---
+++ odev1.dart
@@ -0,0 +1,48 @@
+void main(){
+  String sehir = "Ankara" ;
+  String ulke = "Türkiye" ;
+  final int telNo = 5055052020;
+  final int postaKodu = 06210;
+  String email = "asligvn2000@gmail.com";
+  String meslek = "Mühendis";
+  double stokMiktari = 75.4;
+  String musteriAdi = "Aslıhan";
+  double bakiye = 13.450;
+  var dogumGunu =DateTime(2000,12,06);
+  double maas = 27.300 ;
+  bool evliMi = false;
+  String urunYorum = "Yararlı" ;
+  var odemeTarihi = DateTime(2023, 03,11);
+  String odeme = "Nakit";
+  double siparisAdedi = 1.300;
+  String arabaModel = "SUV";
+  String kitapAdi = "Babaya Mektup";
+  var yayinlanmaTarihi = DateTime(1952, 01, 01);
+  String indirimMiktari = "%45";
+  int odaSayisi = 123 ;
+  String enlem = "39.57K" ;
+  String boylam = "32.53D" ;
+  String urunAdi = "Bilgisayar" ;
+  double yemekFiyati = 73.50 ;
+  String marka = "Monster";
+  String muzikAdi = "Derin" ;
+  double videoSuresi = 15.45;
+  double urunPuani = 7.3;
+  String resimAdi = "Grafiti";
+  String dosyaFormati = ".doc";
+  String renk = "Mor" ;
+  String renkKodu = "#800080";
+  String telModeli = "Apple iPhone 13 128GB";
+  String ekranBoyut = "15.6 inç";
+  double agirlik = 9.80 ;
+  String ulusalGun = "30 Ağustos Zafer Bayramı";
+  var tatilGunu = DateTime(2022,12,24);
+  var rezervasyonTarihi = DateTime(2023, 01, 15);
+  String sokakAdi = "823.sokak";
+  String otobusHatti = "192-1";
+  double kalanDakika = 12.50;
+  int takipKodu = 100987050073;
+  String faturaAdresi = "Ankara/Yenimahalle";
+
+
+}
